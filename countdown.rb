b50d7a539a23a6ec2aa046904d07ba80f97d941e
Jamie Vullo <jvullo@protonmail.com>
2020-07-19 17:48:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,4 +7,13 @@ def countdown(x)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep 
+def countdown_with_sleep(x)
+  sleep(5)
+  x = 10
+  while x > 0 
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+
+end
